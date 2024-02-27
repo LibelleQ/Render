512b095bcde0c000f3023707afdfc00f67a6dbdf
LibelleQ <148550971+LibelleQ@users.noreply.github.com>
2024-02-27 10:59:53 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'dbheruko_j7dw', // Database name
+  '', // Database name
   '', // User
   '', // Password
   {
