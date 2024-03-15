510b56a80bb49056ad38e4b41aa56196b790f9c0
LibelleQ <148550971+LibelleQ@users.noreply.github.com>
2024-03-15 11:11:30 +0100

    new

--- db.js
+++ db.js
@@ -2,11 +2,8 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  process.env.DB_NAME, // Database name
-  process.env.DB_USER, // User
-  process.env.DB_PASSWORD, // Password
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
